8ba331a32c8dab5c2aeeb9193aaf0aa380b3eeed
casey van-roose <kcvanroose@gmail.com>
2018-07-18 14:16:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
   end
   
   def add_student(student, grade)
-   if @roster[grade].empty? 
+   if @roster[grade] == nil
       @roster[grade] = []
     end
       @roster[grade] << student
